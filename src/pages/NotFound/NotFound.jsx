0fe314f8bcda79f88018c8f1b3a38fe8a32ffc8a
Dmtro_Tr <Di4gamegem@gmail.com>
2025-02-01 21:06:48 +0200

    not found

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,12 @@
+import s from "./notFound.module.css";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className={s.wrapper}>
+      <h3 className={s.title}>Sorry</h3>
+      <p className={s.text}>page not found</p>
+    </div>
+  );
 };
 
 export default NotFound;
