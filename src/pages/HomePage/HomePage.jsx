b4106fb209ec1ae3db77648b894cf222ca962f6e
Dmtro_Tr <Di4gamegem@gmail.com>
2025-02-01 20:32:16 +0200

    Home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,21 @@
+import s from "./homePage.module.css";
+
 const HomePage = () => {
   return (
-    <div>
-      <h2>Home page</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
-        exercitationem.
+    <div className={s.wrapper}>
+      <h2 className={s.title}>
+        Welcome to EasyPhonebook <br /> The Smart Way to Store & Find Contacts!
+      </h2>
+      <p className={s.text}>
+        Tired of losing important contacts? With EasyPhonebook, you can easily store, manage, and find your contacts in
+        seconds! Save names and phone numbers securely and search effortlessly whenever you need them.
       </p>
+      <ul className={s.list}>
+        <li>🔹 Quick & Easy Contact Storage</li>
+        <li>🔹 Instant Search by Name or Number</li>
+        <li>🔹 Secure & Reliable</li>
+      </ul>
+      <p className={s.text}>Start organizing your contacts today! Sign up now and never lose a number again! 🚀</p>
     </div>
   );
 };
