0e2f44c9534b25131c4b94a01a35f18fe49c4ead
Dmtro_Tr <Di4gamegem@gmail.com>
2025-02-01 20:51:37 +0200

    logo fixed

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import clsx from "clsx";
 import { selectIsLoggedIn } from "../../redux/auth/selectors";
+import logo from "../../images/phonebook_logo.png";
 import s from "./navigation.module.css";
 
 const Navigation = () => {
@@ -14,7 +15,7 @@ const Navigation = () => {
   return (
     <nav className={s.nav}>
       <NavLink to="/">
-        <img src="../../../public/phonebook_logo.png" alt="logo" width={45} />
+        <img src={logo} alt="" width={45} />
       </NavLink>
       <NavLink className={buildLinkClass} to="/">
         Home
